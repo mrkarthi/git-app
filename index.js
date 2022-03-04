975b5bf19c32352a687087c84874c3594f372241
Karthi M R <Karthi.MR@cognizant.com>
2022-03-04 19:14:17 +0530

    Removed lines 3 - *

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 console.log("Hello Node.js");
 console.log("Hello World..");
-console.log("Welcome to Git..");
-console.log("Testing git pull");
-console.log("Testing git fetch and rebase");
-console.log("Self testing git");
